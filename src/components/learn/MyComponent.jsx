0f2b5517d8f755dca3674ec6f42bd33816b07907
thienthanh1101 <thienthanh1101@gmail.com>
2024-10-25 16:26:06 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,15 @@
+import './stype.css'
+
 //()=>{}
 // component=html + css + javaScript
+//Jsx--> dịch htlm
+//fragment --> cho làm nhiều div <></>
 const MyComponent = () => {
     return (
-        <div>huynv & nguyen</div>
+        <>
+            <div>huynv & nguyen</div>
+            <div className="child" style={{ borderRadius: "10px" }}>Childrent</div>
+        </>
     );
 
 }
