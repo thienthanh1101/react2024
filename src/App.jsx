8fdb0b7735550170f3661b5ad26807d19fdf8108
thienthanh1101 <thienthanh1101@gmail.com>
2024-11-02 13:39:03 +0700

    video 43 delele

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,10 @@ const App = () => {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodo);
+  };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
@@ -31,6 +34,7 @@ const App = () => {
       {todoList.length > 0 ? (
         <TodoData //name={huynv} age={age} data={data}
           todoList={todoList}
+          deleteTodo={deleteTodo}
         />
       ) : (
         <div className="todo-image">
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,16 +1,24 @@
 const TodoData = (props) => {
   //const TodoData = ({ name }) => {
   //props is var object{}
-  const { todoList } = props; // by other const name=props.name; const age=props.age;
+  const { todoList, deleteTodo } = props; // by other const name=props.name; const age=props.age;
   //console.log(">>> Check props:", props);
+  const handleClick = (id) => {
+    deleteTodo(id);
+  };
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {
         console.log(">>> Check map:", item, index);
         return (
           <div className={`todo-Item`} key={item.id}>
-            <div> {item.name}</div>
-            <button>Delete</button>
+            <div>{item.name}</div>
+            <button
+              onClick={() => handleClick(item.id)}
+              style={{ cursor: "pointer" }}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
