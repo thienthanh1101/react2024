5e54af5a874711567af0c6d8e024a8ffdf51f9ed
thienthanh1101 <thienthanh1101@gmail.com>
2024-11-17 18:01:28 +0700

    video 68

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,28 +1,39 @@
 import { Button, Input } from "antd";
-
-const UserForm = () =>{
-    return(
-       <div className="user-form" style={{margin:"20px 0"}}>
-        <div style={{display: "flex", gap: "10px", flexDirection:"column"}}>
-            <div>
-                <span>FullName</span>
-                <Input/>
+function UserForm() {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const handleClickBtn = () => {
+        console.log(">>> check state: ", { fullName, email, password, phone });
+    };
+    return (
+        <div className="user-form" style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+                <div>
+                    <span>FullName</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password />
+                </div>
+                <div>
+                    <span>Phone Number</span>
+                    <Input />
+                </div>
+                <div>
+                    <Button
+                        // onClick={() => handleClickBtn()}
+                        onClick={handleClickBtn}
+                        type="primary"> Create User </Button>
+                </div>
             </div>
-            <div>
-                <span>Email</span>
-                <Input/>
-            </div>
-            <div>
-                <span>Password</span>
-                <Input.Password/>
-            </div>
-            <div>
-                <span>Phone Number</span>
-                <Input/>
-            </div>
-            <div><Button type="primary">Create user</Button></div>
         </div>
-       </div>
-    )
+    );
 }
 export default UserForm;
