5e323b4728b854751f22b1d4ff1ea925bbfb0ba7
thienthanh1101 <thienthanh1101@gmail.com>
2024-11-28 23:02:50 +0700

    video 82 add avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -2,7 +2,9 @@ import { Drawer } from "antd"
 const ViewUserDetail =(props)=>{
     const{dataDetail,setDataDetail,isDetailOpen,setIsDetailOpen}=props;
     return (
-        <Drawer title="Chi tiết User"
+        <Drawer 
+        width={"30vw"}
+        title="Chi tiết User"
         onClose={() => {
             setDataDetail(null);
             setIsDetailOpen(false);
@@ -17,6 +19,25 @@ const ViewUserDetail =(props)=>{
             <p>Email: {dataDetail.email}</p>
             <br />
             <p>Phone number: {dataDetail.phone}</p>
+            <p>Avatar:</p>
+            <div>
+                <img height={150} width={150}  
+                src= {`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}/>
+            </div>
+            <div>
+                    <label htmlFor='btnUpload' style={{
+                        display: "block",
+                        width: "fit-content",
+                        marginTop: "15px",
+                        padding: "5px 10px",
+                        background: "orange",
+                        borderRadius: "5px",
+                        cursor: "pointer"
+                    }}>
+                        Upload Avatar
+                    </label>
+                    <input type='file' hidden id='btnUpload' />
+            </div>
         </>
             :
             <>
